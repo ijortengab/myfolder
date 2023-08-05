c6e7861ecae5b8a4a71951b3bc9b8fffa0ae1a55
IjorTengab <m_roji28@yahoo.com>
2023-08-05 07:20:47 +0700

    [index.php] Human readble pada kolom size dan date. Route sekarang mendukung placeholder.

--- index.php
+++ index.php
@@ -351,16 +351,8 @@ class JsonResponse extends Response {
     }
 }
 class Application {
+    public static $request;
     protected $register = array();
-    public $request;
-    /**
-     *
-     */
-    public function __construct()
-    {
-        $this->request = new Request;
-        // return $this;
-    }
     /**
      *
      */
@@ -373,45 +365,92 @@ class Application {
      */
     public function get($pathinfo, $callback)
     {
+        // @todo, jika ada yang kayak gini:
+        // $app->get('/___pseudo/target_directory/{sch|eme}', 'IjorTengab\MyFolder\Controller::pseudoHandle');
+        // maka sejak awal sudah dikasih throw exception aja.
+        // karena gak valid sebagai placeholder.
         $this->register['get'][$pathinfo] = $callback;
     }
     /**
      *
      */
+    public static function getRequest()
+    {
+        if (null === self::$request) {
+            self::$request = new Request;
+        }
+        return self::$request;
+    }
+    /**
+     * @todo, bagaimana jika path_info terdapat karakter %20. apakah perlu urldecode?.
+     */
     public function run()
     {
-        $request_method = strtolower($this->request->server->get('REQUEST_METHOD'));
-        $path_info = $this->request->getPathInfo();
+        $request = self::getRequest();
+        $request_method = strtolower($request->server->get('REQUEST_METHOD'));
+        $path_info = $request->getPathInfo();
         if (!isset($this->register[$request_method])) {
             throw new Exception('Request Method not found.');
         }
         $register = $this->register[$request_method];
-        // @todo, /__pseudo/{path} dibuat dapat diparsing.
-        $options = array();
-        if ($path_info == '/__pseudo/script.js') {
-            $callback = $register['/__pseudo/{path}'];
-            $options['path'] = 'script.js';
+        do {
+            // Filter berdasarkan fix string.
+            $register_filtered = array_filter($register, function ($key) use ($path_info) {
+                return $path_info === $key;
+            }, ARRAY_FILTER_USE_KEY);
+            if ($register_filtered) {
+                $key = key($register_filtered);
+                $callback = $register_filtered[$key];
+                $args = array();
+                break;
+            }
+            // Filter berdasarkan regex.
+            $register_filtered = array_filter($register, function ($key) use ($path_info) {
+                $pattern_parts = preg_split('/\{[^}]+\}/', $key);
+                $pattern_quoted_parts = array_map(function ($value) {
+                    return preg_quote($value,'/');
+                }, $pattern_parts);
+                $pattern_quoted = implode('[^\/]+', $pattern_quoted_parts);
+                return preg_match('/^'.$pattern_quoted.'$/', $path_info);
+            }, ARRAY_FILTER_USE_KEY);
+            if ($register_filtered) {
+                $key = key($register_filtered);
+                $save = array();
+                $pattern = preg_replace_callback('/\{[^}]+\}/', function ($matches) use (&$save) {
+                    $value = array_shift($matches);
+                    $name = trim($value, '{}');
+                    $save[] = $name;
+                    return '(?P<'.$name.'>.+)';
+                }, $key);
+                $pattern_parts = preg_split('/\(\?P\<[a-z]+\>\.\+\)/', $pattern);
+                $pattern_quoted_parts = array_map(function ($value, $name) {
+                    if (!empty($name)) {
+                        $name = '(?P<'.$name.'>.+)';
+                    }
+                    return preg_quote($value,'/').$name;
+                }, $pattern_parts, $save);
+                $pattern_quoted = implode('', $pattern_quoted_parts);
+                preg_match('/^'.$pattern_quoted.'/', $path_info, $matches);
+                $args = array_filter($matches, function ($key) {
+                    return !is_numeric($key);
+                }, ARRAY_FILTER_USE_KEY);
+                $callback = $register_filtered[$key];
+                break;
+            }
         }
-        else {
-            $callback = $this->register[$request_method]['/'];
+        while (false);
+        if (empty($callback)) {
+            $callback = $register['/'];
+            $args = array();
         }
-        call_user_func_array($callback, array($this, $options));
+        call_user_func_array($callback, $args);
     }
 }
 class Controller {
-    /**
-     *
-     */
-    public function __construct()
+    public static function index()
     {
-    }
-    /**
-     *
-     */
-    public static function getHandle(Application $app)
-    {
-        $target_directory = &Config::$target_directory;
-        $request = $app->request;
+        $target_directory = Config::$target_directory;
+        $request = Application::getRequest();
         $path_info = $request->getPathInfo();
         $base_path = $request->getBasePath();
         $fullpath = $target_directory.$path_info;
@@ -423,8 +462,7 @@ class Controller {
             }
         }
         if (is_file($fullpath)) {
-            $file = $fullpath;
-            $response = new BinaryFileResponse($file);
+            $response = new BinaryFileResponse($fullpath);
             return $response->send();
         }
         $config = array(
@@ -432,7 +470,7 @@ class Controller {
             'base_path' => $request->getBasePath(),
         );
         $config_json = json_encode($config);
-        $content = strtr(TemplateFile::autoIndex(), array(
+        $content = strtr(TemplateFile::indexHtml(), array(
             '{{ config.base }}' => $config_json,
             '{{ config.base_path }}' => $base_path,
             ));
@@ -442,13 +480,14 @@ class Controller {
     /**
      *
      */
-    public static function postHandle(Application $app)
+    public static function ajax()
     {
-        $target_directory = &Config::$target_directory;
-        if ($app->request->request->has('action')) {
+        $target_directory = Config::$target_directory;
+        $request = Application::getRequest();
+        if ($request->request->has('action')) {
             // @todo: Jika tidak ada $_POST['directory'], maka throw error.
-            $current_directory = $target_directory.$app->request->request->get('directory');
-            $action = $app->request->request->get('action');
+            $current_directory = $target_directory.$request->request->get('directory');
+            $action = $request->request->get('action');
             $list_directory = scandir($current_directory);
             $list_directory = array_diff($list_directory, array('.','..'));
             switch ($action) {
@@ -497,19 +536,14 @@ class Controller {
             }
         }
     }
-    /**
-     *
-     */
-    public static function pseudoHandle(Application $app, $args = array())
+}
+class PseudoController extends Controller {
+    public static function getFile($file)
     {
-        $path = isset($args['path']) ? $args['path']: null;
-        switch ($path) {
+        switch ($file) {
             case 'script.js':
                 header('Content-Type: application/javascript; charset=utf-8');
-                $content = strtr(PseudoFile::scriptJs(), array(
-                    '{{ config.base }}' => $config_json,
-                    '{{ config.base_path }}' => $base_path,
-                    ));
+                $content = TemplateFile::scriptJs();
                 $response = new Response($content);
                 $response->send();
                 break;
@@ -521,9 +555,30 @@ class Controller {
                 break;
         }
     }
+    public static function getTargetDirectoryFile($scheme)
+    {
+        switch ($scheme) {
+            case 'public':
+                $target_directory = Config::$target_directory;
+                $request = Application::getRequest();
+                $path = $request->query->get('path');
+                $fullpath = $target_directory.$path;
+                if (is_file($fullpath)) {
+                    $response = new BinaryFileResponse($fullpath);
+                    return $response->send();
+                }
+                break;
+            case '':
+                // Do something.
+                break;
+            default:
+                // Do something.
+                break;
+        }
+    }
 }
 class TemplateFile {
-    public static function autoIndex()
+    public static function indexHtml()
     {
         return <<<'EOF'
 <!DOCTYPE html>
@@ -586,14 +641,12 @@ class TemplateFile {
 var MyFolder = MyFolder || {config:{}}
 MyFolder.config = JSON.parse('{{ config.base }}')
 </script>
-<script src="{{ config.base_path }}/__pseudo/script.js">
+<script src="{{ config.base_path }}/___pseudo/script.js">
 </script>
 </body>
 </html>
 EOF;
     }
-}
-class PseudoFile {
     /**
      *
      */
@@ -651,6 +704,126 @@ function drawColumnName(data) {
     // }, 2000);
     return defer;
 }
+// Credit:
+// https://stackoverflow.com/questions/10420352/converting-file-size-in-bytes-to-human-readable-string/20732091#20732091
+function humanFileSize(size) {
+    var i = size == 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
+    return (size / Math.pow(1024, i)).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
+}
+// Credit:
+// - https://gist.github.com/kubiqsk/c60207a3075104df7cc1822a95053ecd
+(function(){
+	var replaceChars = {
+		// day
+		d: function(){ return ( '0' + this.getDate() ).slice(-2) },
+		D: function( locale ){ return new Intl.DateTimeFormat( locale, { weekday: 'short' } ).format( this ) },
+		j: function(){ return this.getDate() },
+		l: function( locale ){ return new Intl.DateTimeFormat( locale, { weekday: 'long' } ).format( this ) },
+		N: function(){
+			let day = this.getDay();
+			return day === 0 ? 7 : day;
+		},
+		S: function(){
+			let date = this.getDate();
+			return date % 10 === 1 && date !== 11 ? 'st' : ( date % 10 === 2 && date !== 12 ? 'nd' : ( date % 10 === 3 && date !== 13 ? 'rd' : 'th' ) );
+		},
+		w: function(){ return this.getDay() },
+		z: function(){ return Math.floor( ( this - new Date( this.getFullYear(), 0, 1 ) ) / 86400000 ) },
+		// week
+		W: function(){
+			let target = new Date( this.valueOf() );
+			let dayNr = ( this.getDay() + 6 ) % 7;
+			target.setDate( target.getDate() - dayNr + 3 );
+			let firstThursday = target.valueOf();
+			target.setMonth( 0, 1 );
+			if( target.getDay() !== 4 ){
+				target.setMonth( 0, 1 + ( ( 4 - target.getDay() ) + 7 ) % 7 );
+			}
+			return Math.ceil( ( firstThursday - target ) / 604800000 ) + 1;
+		},
+		// month
+		F: function( locale ){ return new Intl.DateTimeFormat( locale, { month: 'long' } ).format( this ) },
+		m: function(){ return ( '0' + ( this.getMonth() + 1 ) ).slice(-2) },
+		M: function( locale ){ return new Intl.DateTimeFormat( locale, { month: 'short' } ).format( this ) },
+		n: function(){ return this.getMonth() + 1 },
+		t: function(){
+			let year = this.getFullYear();
+			let nextMonth = this.getMonth() + 1;
+			if( nextMonth === 12 ){
+				year = year++;
+				nextMonth = 0;
+			}
+			return new Date( year, nextMonth, 0 ).getDate();
+		},
+		// year
+		L: function(){
+			let year = this.getFullYear();
+			return year % 400 === 0 || ( year % 100 !== 0 && year % 4 === 0 ) ? 1 : 0;
+		},
+		o: function(){
+			let date = new Date( this.valueOf() );
+			date.setDate( date.getDate() - ( ( this.getDay() + 6 ) % 7 ) + 3 );
+			return date.getFullYear();
+		},
+		Y: function(){ return this.getFullYear() },
+		y: function(){ return ( '' + this.getFullYear() ).slice(-2) },
+		// time
+		a: function(){ return this.getHours() < 12 ? 'am' : 'pm' },
+		A: function(){ return this.getHours() < 12 ? 'AM' : 'PM' },
+		B: function(){
+			return ( '00' + Math.floor( ( ( ( this.getUTCHours() + 1 ) % 24 ) + this.getUTCMinutes() / 60 + this.getUTCSeconds() / 3600 ) * 1000 / 24 ) ).slice(-3);
+		},
+		g: function(){ return this.getHours() % 12 || 12 },
+		G: function(){ return this.getHours() },
+		h: function(){ return ( '0' + ( this.getHours() % 12 || 12 ) ).slice(-2) },
+		H: function(){ return ( '0' + this.getHours() ).slice(-2) },
+		i: function(){ return ( '0' + this.getMinutes() ).slice(-2) },
+		s: function(){ return ( '0' + this.getSeconds() ).slice(-2) },
+		v: function(){ return ( '00' + this.getMilliseconds() ).slice(-3) },
+		// Timezone
+		e: function(){ return Intl.DateTimeFormat().resolvedOptions().timeZone },
+		I: function(){
+			let DST = null;
+			for( let i = 0; i < 12; ++i ){
+				let d = new Date( this.getFullYear(), i, 1 );
+				let offset = d.getTimezoneOffset();
+				if( DST === null ){
+					DST = offset;
+				}else if( offset < DST ){
+					DST = offset;
+					break;
+				}else if( offset > DST ){
+					break;
+				}
+			}
+			return ( this.getTimezoneOffset() === DST ) | 0;
+		},
+		O: function(){
+			let timezoneOffset = this.getTimezoneOffset();
+			return ( -timezoneOffset < 0 ? '-' : '+' ) + ( '0' + Math.floor( Math.abs( timezoneOffset / 60 ) ) ).slice(-2) + ( '0' + Math.abs( timezoneOffset % 60 ) ).slice(-2);
+		},
+		P: function(){
+			let timezoneOffset = this.getTimezoneOffset();
+			return ( -timezoneOffset < 0 ? '-' : '+' ) + ( '0' + Math.floor( Math.abs( timezoneOffset / 60 ) ) ).slice(-2) + ':' + ( '0' + Math.abs( timezoneOffset % 60 ) ).slice(-2);
+		},
+		T: function( locale ){
+			let timeString = this.toLocaleTimeString( locale, { timeZoneName: 'short' } ).split(' ');
+			let abbr = timeString[ timeString.length - 1 ];
+			return abbr == 'GMT+1' ? 'CET' : ( abbr == 'GMT+2' ? 'CEST' : abbr );
+		},
+		Z: function(){ return -this.getTimezoneOffset() * 60 },
+		// Full Date/Time
+		c: function(){ return this.format('Y-m-d\\TH:i:sP') },
+		r: function(){ return this.format('D, d M Y H:i:s O') },
+		U: function(){ return Math.floor( this.getTime() / 1000 ) }
+	}
+	Date.prototype.format = function( formatStr, locale = navigator.language ){
+		var date = this;
+		return formatStr.replace( /(\\?)(.)/g, function( _, esc, chr ){
+			return esc === '' && replaceChars[ chr ] ? replaceChars[ chr ].call( date, locale ) : chr
+		})
+	}
+}).call( this );
 function drawColumnOther(data) {
     console.log('drawColumnOther()');
     var $table = $('#table-main');
@@ -660,8 +833,6 @@ function drawColumnOther(data) {
         $tbody.find('tr').filter(function (i) {
             var $this = $(this);
             if ($this.data('info') == info.name) {
-                $this.find("td.mtime").text(info.mtime)
-                $this.find("td.size").text(info.size)
                 if (info.type == '.') {
                     $this.find("td.type").text('File folder')
                     var $a = $this.find("td > a");
@@ -672,6 +843,10 @@ function drawColumnOther(data) {
                     $a.data('type',info.type);
                 }
                 else {
+                    let ms = info.mtime * 1000
+                    let d = new Date(ms)
+                    $this.find("td.mtime").text(d.format('Y-m-d H:i:s'))
+                    $this.find("td.size").text(humanFileSize(info.size))
                     var $a = $this.find("td > a");
                     var biclass = getClassByType(info.type)
                     if (biclass != '') {
@@ -680,7 +855,12 @@ function drawColumnOther(data) {
                     else {
                         $a.before('<i class="bi bi-filetype-'+info.type+'"></i> ');
                     }
-                    $this.find("td.type").text(info.type)
+                    $this.find("td.type").text(info.type.toUpperCase()+' File')
+                    // Array.prototype.includes() not support for old browser.
+                    var extensionReadByPHP = ['php', 'htaccess'];
+                    if (extensionReadByPHP.includes(info.type.toLowerCase())) {
+                        $a.attr('href', MyFolder.config.base_path+'/___pseudo/target_directory/public?path='+MyFolder.config.path_info+info.name);
+                    }
                 }
             }
         });
@@ -760,8 +940,9 @@ EOF;
     }
 }
 $app = new Application;
-$app->get('/', 'IjorTengab\MyFolder\Controller::getHandle');
-$app->post('/', 'IjorTengab\MyFolder\Controller::postHandle');
-$app->get('/__pseudo/{path}', 'IjorTengab\MyFolder\Controller::pseudoHandle');
+$app->get('/', 'IjorTengab\MyFolder\Controller::index');
+$app->post('/', 'IjorTengab\MyFolder\Controller::ajax');
+$app->get('/___pseudo/{file}', 'IjorTengab\MyFolder\PseudoController::getFile');
+$app->get('/___pseudo/target_directory/{scheme}', 'IjorTengab\MyFolder\PseudoController::getTargetDirectoryFile');
 $app->run();
 ?>
