f28651f54661fcc52606855317bb4431ded8bf40
ijortengab <m_roji28@yahoo.com>
2024-02-02 09:01:44 +0700

    Menambah event baru Core/HtmlWrapperHtmlElementEvent.

---
+++ src/MyFolder/Core/HtmlWrapperHtmlElementEvent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace IjorTengab\MyFolder\Core;
+
+class HtmlWrapperHtmlElementEvent extends HtmlElementEvent
+{
+    const NAME = 'core.html_wrapper_html_element.event';
+
+    // Disable line below.
+    // Sehingga static::load() akan menggunakaninstance dari parent
+    // (HtmlElementEvent).
+    // protected static $instance;
+}
