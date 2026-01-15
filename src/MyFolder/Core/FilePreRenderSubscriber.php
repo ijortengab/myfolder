718bebbd2dfde723cf442ac981092c8625021062
ijortengab <m_roji28@yahoo.com>
2026-01-15 13:49:40 +0700

    Core. Code cleaning: FilePreRenderSubscriber.php.

--- src/MyFolder/Core/FilePreRenderSubscriber.php
+++ src/MyFolder/Core/FilePreRenderSubscriber.php
@@ -2,13 +2,9 @@
 
 namespace IjorTengab\MyFolder\Core;
 
-use IjorTengab\MyFolder\Core\Application;
 use IjorTengab\MyFolder\Core\EventSubscriberInterface;
 use IjorTengab\MyFolder\Core\FilePreRenderEvent;
-use IjorTengab\MyFolder\Core\Response;
-use IjorTengab\MyFolder\Core\RedirectResponse;
 use IjorTengab\MyFolder\Core\BinaryFileResponse;
-use IjorTengab\MyFolder\Core\ConfigHelper;
 
 class FilePreRenderSubscriber implements EventSubscriberInterface
 {
